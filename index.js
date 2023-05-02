f733ac2d8a365a078d71ba24d7e95f7e5e8dc172
shamim26 <shamimreja101@gmail.com>
2023-05-02 16:20:22 +0600

    server side added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5180;
+
+const chefData = require("./data/chef.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chefData);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefData.find((chef) => chef.chefId === parseInt(id));
+  res.send(selectedChef);
+});
+
+app.listen(port, () => {
+  console.log(`listening from port ${port}`);
+});
